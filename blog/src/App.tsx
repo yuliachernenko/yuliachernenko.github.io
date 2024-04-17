6f855e33c8c9eb0be5ee9ee3f6fde922c77f4af9
chernenkojulia77 <chernenkojulia77@gmail.com>
2024-04-18 00:08:19 +0300

    added exercise33

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -3,35 +3,42 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+//Завдання 3
 const jsxEL = <h1> I am a JSX element</h1>;
-// const Header = (
-//   <header>
-//     <h1> Hello world</h1>
-//     <h2>Hi there</h2>
-//     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus dolore unde quas eligendi fugiat facilis cupiditate eos ratione repudiandae vero nulla dolores quisquam ducimus, rem maxime officiis earum consequatur fuga.</p>
-// </header>
-// )
-    
-// const H1 = <h1> Hello world</h1>;
-// const H2 = <h2>Hi there</h2>;
 
-const data = {
-  H1: <h1> Hello world</h1>,
-  H2: <h2>Hi there</h2>
-}
+//Завдання 4
 const Header = (
   <header>
-    {data.H1}
-    {data.H2}
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus dolore unde quas eligendi fugiat facilis cupiditate eos ratione repudiandae vero nulla dolores quisquam ducimus, rem maxime officiis earum consequatur fuga.</p>
+  <h1>Welcome to React</h1>
+  <h2>Getting Started React</h2>
+  <h3>JavaScript Library</h3>
 </header>
 )
+//Завдання 5
+const JSXfragment = (
+  <>
+ <h1>Welcome to React</h1>
+ <h2>Getting Started React</h2>
+ <h3>JavaScript Library</h3>
+</>
+
+)
+
+//Завдання 6
+const Footer = (
+  <footer>
+    <p>Copyright &copy; 2024</p>
+ </footer>
+)
+    
 
 function App() {
   const [count, setCount] = useState(0)
 let today = new Date()
   return (
     <>
+      {Header}
+      
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -52,13 +59,15 @@ let today = new Date()
 
       {jsxEL}
 
-      {Header}
+      {JSXfragment}
 
       <h3 style={{backgroundColor: 'blue', color:'gold'}}>It's today: {today.toLocaleDateString()}</h3>
 
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      {Footer}
     </>
   )
 }
